50228e264a873ff0f0bec31a8c8d7bb6480f0c13
nailashafiya <nai27.sf@gmail.com>
2025-05-22 21:42:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("🐕Tralala")
 st.write(
     "haii hai")
-st.image("9e4a8a9129740b2f047e2fbece8485b6.jpg", widht=200)
+st.image("png-clipart-aristocats-marie-illustration-marie-cat-drawing-youtube-mary-white-mammal.png", widht=200)
