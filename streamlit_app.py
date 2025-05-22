c7ea8b47a993f0f366b5b88290184345d69ff9ea
nailashafiya <nai27.sf@gmail.com>
2025-05-22 21:40:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("🐕Tralala")
 st.write(
     "haii hai")
-st.image("1d587fe77b8554f82cfef273006106a8 (1).jpg", widht=200)
+st.image("9e4a8a9129740b2f047e2fbece8485b6.jpg", widht=200)
